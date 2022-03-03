158fd4f31a671f8d82e76b7591b6bf1b51f1299f
Emili Kaiman <emilikaiman@gmail.com>
2022-03-03 13:46:47 -0700

    Add function to populate locations for dropdown

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,10 +1,34 @@
 const domUpdates = {
-  updateTotalSpent(total){
-    let totalSpent = document.getElementById('totalSpent')
-    totalSpent.innerText += ` $${total}`
-    console.log(total)
-    console.log('dom update')
-  }
-}
+  updateTotalSpent(total) {
+    let totalSpent = document.getElementById("totalSpent");
+    totalSpent.innerText += ` $${total}`;
+    console.log(total);
+    console.log("dom update");
+  },
 
-export default domUpdates
+  displayAllTrips(allTrips, destinationData) {
+    let allTripsWrapper = document.getElementById("allTripsWrapper");
+    allTrips.forEach((trip) => {
+      destinationData.forEach((destination) => {
+
+        if(trip.destinationID === destination.id){
+
+          console.log(trip.destinationID, 'ID')
+          allTripsWrapper.innerHTML += `<div class="oneTrip">
+          <p id="destinatio">${destination.destination}</p>
+          <img src='${destination.image}' alt="${destination.alt}">
+          </div>`;
+        }
+      });
+    });
+  },
+
+  displayDropDownOptions(destinationData){
+    let dropDownDestinations = document.getElementById("dropDownDestinations")
+    destinationData.forEach(destination => {
+      dropDownDestinations.innerHTML += `<option value="${destination.destination}">${destination.destination}</option>`
+    })
+  }
+};
+
+export default domUpdates;
