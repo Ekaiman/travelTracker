904da8853a9a1b3e4928e6137fdcf6569cb2a8b7
Emili Kaiman <emilikaiman@gmail.com>
2022-03-02 19:53:52 -0700

    Create promise all and invoke dom updates

--- src/scripts.js
+++ src/scripts.js
@@ -1,13 +1,44 @@
 // This is the JavaScript entry file - your code begins here
 // Do not delete or rename this file ********
+import domUpdates from "./domUpdates";
+import fetchData from "./apiCalls.js";
+import UserRepository from "./UserRepository";
+import User from "./User";
+import Trips from "./Trips";
+import Destination from "./Destinations";
 
+let totalSpent = document.getElementById('totalSpent')
 // An example of how you tell webpack to use a CSS (SCSS) file
-import './css/base.scss';
+// import './css/base.scss';
+let tripInst;
+let total;
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// import './images/turing-logo.png'
+
+// console.log('This is the JavaScript entry file - your code begins here.');
+const newInstanceTrip = (id, tripData) => {
+  tripInst = new Trips(id, tripData)
+}
+
+let fetchAllData = () => {
+  Promise.all([
+    fetchData("http://localhost:3001/api/v1/travelers"),
+    fetchData("http://localhost:3001/api/v1/trips"),
+    fetchData("http://localhost:3001/api/v1/destinations"),
+  ]).then(data => {
+    const travelersData = data[0].travelers
+    const tripsData = data[1].trips
+    const destinationData = data[2].destinations
+    newInstanceTrip(35, tripsData)
+    total = tripInst.getCostOfTripsThisYear(destinationData)
+    domUpdates.updateTotalSpent(total)
+  })
+};
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
+const loadPage = () => {
+  fetchAllData();
+};
 
->>>>TEST<<<<
+window.onload = loadPage;
