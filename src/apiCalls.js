8323821e78d5771ecc7d438c679e83ca141aff6c
Emili Kaiman <emilikaiman@gmail.com>
2022-03-04 09:14:58 -0700

    Create post function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -4,8 +4,36 @@ import domUpdates from "./domUpdates";
 
 const fetchData = (url) => {
   return fetch(url)
-  .then((response) => response.json())
-  .catch((error) => console.log(error));
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
 };
 
-export default fetchData
+const postData = (selectedLocationObj, getTripInformation) => {
+  return fetch("http://localhost:3001/api/v1/trips", {
+    method: "POST",
+    body: JSON.stringify({
+      id: getTripInformation.tripId,
+      userID: 44,
+      destinationID: selectedLocationObj.id,
+      travelers: getTripInformation.travelers,
+      date: getTripInformation.dateFixed,
+      duration: getTripInformation.duration,
+      status: "pending",
+      suggestedActivities: [],
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then(response => checkOk(response))
+};
+
+const checkOk = (response) => {
+  if (!response.ok){
+    // throw new Error(respose)
+    throw `${response.status} ${response.statusText}`
+  } else {
+    response.json()
+  }
+}
+
+export { fetchData, postData };
