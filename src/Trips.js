bf5e9e358b029d471516ef5269f7e8060e58b29e
Emili Kaiman <emilikaiman@gmail.com>
2022-03-04 13:07:04 -0700

    Change dates to numbers and sort trips by date

--- src/Trips.js
+++ src/Trips.js
@@ -1,36 +1,68 @@
-class Trips{
-  constructor(id, tripData){
-    this.tripData = tripData
-    this.tripDataOneUser = tripData.filter(trip => trip.userID === id)
+class Trips {
+  constructor(id, tripData) {
+    this.tripData = tripData;
+    this.tripDataOneUser = tripData.filter((trip) => trip.userID === id);
+    this.tripDataNumberDates;
   }
 
-  returnDestinationId(){
-    return this.tripDataOneUser.map(trip => trip.destinationID)
+  returnDestinationId() {
+    return this.tripDataOneUser.map((trip) => trip.destinationID);
   }
 
-  getCostOfOneTrip(id, lodgingCost, flightCost){
-    const oneTrip = this.tripDataOneUser.find(trip => trip.id === id)
-    const dailyLodgingCost = oneTrip.duration * lodgingCost
-    const flightCostTotal = oneTrip.travelers * flightCost
-    const totalCost = dailyLodgingCost + flightCostTotal
-    return totalCost
+  getCostOfOneTrip(id, lodgingCost, flightCost) {
+    const oneTrip = this.tripDataOneUser.find((trip) => trip.id === id);
+    const dailyLodgingCost = oneTrip.duration * lodgingCost;
+    const flightCostTotal = oneTrip.travelers * flightCost;
+    const totalCost = dailyLodgingCost + flightCostTotal;
+    return totalCost;
   }
 
   getCostOfTripsThisYear(destinationData) {
-    const thisYearsTrips = this.tripDataOneUser.filter(trip => trip.date.includes('2022'))
+    const thisYearsTrips = this.tripDataOneUser.filter((trip) =>
+      trip.date.includes("2022")
+    );
     let result = destinationData.reduce((sum, currDestination) => {
-      thisYearsTrips.forEach(trip => {
-        if (trip.destinationID === currDestination.id){
-          sum += (trip.duration * currDestination.estimatedLodgingCostPerDay)
-          sum += (trip.travelers * currDestination.estimatedFlightCostPerPerson)
+      thisYearsTrips.forEach((trip) => {
+        if (trip.destinationID === currDestination.id) {
+          sum += trip.duration * currDestination.estimatedLodgingCostPerDay;
+          sum += trip.travelers * currDestination.estimatedFlightCostPerPerson;
         }
-      })
-      return sum
-    }, 0)
-    let agentFee = 1.1 * result
-    return Math.round(agentFee)
+      });
+      return sum;
+    }, 0);
+    let agentFee = 1.1 * result;
+    return Math.round(agentFee);
   }
 
+  turnDatesToNumber() {
+    let dateAsNumbers = this.tripDataOneUser.map((trip) => {
+      let noSlashes = trip.date.split("/").join("");
+      trip.date = parseInt(noSlashes);
+      return trip;
+    });
+    this.tripDataNumberDates = dateAsNumbers;
+    return dateAsNumbers;
+  }
 
+  sortedTrips() {
+    this.turnDatesToNumber()
+    let past = [];
+    let pending = [];
+    let future = [];
+    return this.tripDataNumberDates.reduce((obj, currTrip) => {
+      console.log(currTrip.date);
+      if (currTrip.status === "pending") {
+        pending.push(currTrip);
+      } else if (currTrip.date > 20220304) {
+        future.push(currTrip);
+      } else if (currTrip.date < 20220304) {
+        past.push(currTrip);
+      }
+      obj.past = past;
+      obj.pending = pending;
+      obj.future = future;
+      return obj;
+    }, {});
+  }
 }
-export default Trips
+export default Trips;
--- test/Trips-test.js
+++ test/Trips-test.js
@@ -3,105 +3,113 @@ const expect = chai.expect;
 import User from "../src/User";
 import UserRepository from "../src/UserRepository";
 import Destinations from "../src/Destinations";
-import Trips from "../src/Trips"
-import tripData from "../src/tripData"
-import destinationData from "../src/destinationData"
+import Trips from "../src/Trips";
+import tripData from "../src/tripData";
+import destinationData from "../src/destinationData";
 
 describe("Trips", () => {
   let tripData1;
   let trip1;
   let trip2;
+  let trip3;
 
   beforeEach(() => {
     tripData1 = [
-
-    {
-      "id": 1,
-      "userID": 44,
-      "destinationID": 49,
-      "travelers": 1,
-      "date": "2022/09/16",
-      "duration": 8,
-      "status": "approved",
-      "suggestedActivities": []
-    },
-    {
-      "id": 2,
-      "userID": 35,
-      "destinationID": 25,
-      "travelers": 5,
-      "date": "2022/10/04",
-      "duration": 18,
-      "status": "approved",
-      "suggestedActivities": []
-    }, {
-      "id": 3,
-      "userID": 35,
-      "destinationID": 22,
-      "travelers": 4,
-      "date": "2022/05/22",
-      "duration": 17,
-      "status": "approved",
-      "suggestedActivities": []
-    }, {
-      "id": 4,
-      "userID": 43,
-      "destinationID": 14,
-      "travelers": 2,
-      "date": "2022/02/25",
-      "duration": 10,
-      "status": "approved",
-      "suggestedActivities": []
-    }, {
-      "id": 5,
-      "userID": 42,
-      "destinationID": 29,
-      "travelers": 3,
-      "date": "2022/04/30",
-      "duration": 18,
-      "status": "approved",
-      "suggestedActivities": []
-    }, {
-      "id": 6,
-      "userID": 29,
-      "destinationID": 35,
-      "travelers": 3,
-      "date": "2022/06/29",
-      "duration": 9,
-      "status": "approved",
-      "suggestedActivities": []
-    }, {
-      "id": 7,
-      "userID": 35,
-      "destinationID": 17,
-      "travelers": 5,
-      "date": "2022/5/28",
-      "duration": 20,
-      "status": "approved",
-      "suggestedActivities": []
-    }, {
-      "id": 8,
-      "userID": 36,
-      "destinationID": 39,
-      "travelers": 6,
-      "date": "2022/02/07",
-      "duration": 4,
-      "status": "approved",
-      "suggestedActivities": []
-    }, {
-      "id": 9,
-      "userID": 24,
-      "destinationID": 19,
-      "travelers": 5,
-      "date": "2022/12/19",
-      "duration": 19,
-      "status": "approved",
-      "suggestedActivities": []
-    }
+      {
+        id: 1,
+        userID: 44,
+        destinationID: 49,
+        travelers: 1,
+        date: "2022/09/16",
+        duration: 8,
+        status: "approved",
+        suggestedActivities: [],
+      },
+      {
+        id: 2,
+        userID: 35,
+        destinationID: 25,
+        travelers: 5,
+        date: "2022/10/04",
+        duration: 18,
+        status: "approved",
+        suggestedActivities: [],
+      },
+      {
+        id: 3,
+        userID: 35,
+        destinationID: 22,
+        travelers: 4,
+        date: "2022/05/22",
+        duration: 17,
+        status: "approved",
+        suggestedActivities: [],
+      },
+      {
+        id: 4,
+        userID: 3,
+        destinationID: 14,
+        travelers: 2,
+        date: "2021/02/25",
+        duration: 10,
+        status: "approved",
+        suggestedActivities: [],
+      },
+      {
+        id: 5,
+        userID: 3,
+        destinationID: 29,
+        travelers: 3,
+        date: "2022/04/30",
+        duration: 18,
+        status: "approved",
+        suggestedActivities: [],
+      },
+      {
+        id: 6,
+        userID: 3,
+        destinationID: 35,
+        travelers: 3,
+        date: "2022/06/29",
+        duration: 9,
+        status: "pending",
+        suggestedActivities: [],
+      },
+      {
+        id: 7,
+        userID: 35,
+        destinationID: 17,
+        travelers: 5,
+        date: "2022/5/28",
+        duration: 20,
+        status: "approved",
+        suggestedActivities: [],
+      },
+      {
+        id: 8,
+        userID: 36,
+        destinationID: 39,
+        travelers: 6,
+        date: "2022/02/07",
+        duration: 4,
+        status: "approved",
+        suggestedActivities: [],
+      },
+      {
+        id: 9,
+        userID: 3,
+        destinationID: 19,
+        travelers: 5,
+        date: "2022/12/19",
+        duration: 19,
+        status: "pending",
+        suggestedActivities: [],
+      },
     ];
 
     trip1 = new Trips(44, tripData);
-    trip2 = new Trips(35, tripData1)
+    trip2 = new Trips(35, tripData1);
+    trip3 = new Trips(3, tripData1);
   });
 
   it("should be a function", () => {
@@ -112,26 +120,36 @@ describe("Trips", () => {
     expect(trip1).to.be.an.instanceOf(Trips);
   });
 
-  it('should accept destination data and hold it', () => {
-    expect(trip1.tripData).to.eql(tripData)
-  })
+  it("should accept destination data and hold it", () => {
+    expect(trip1.tripData).to.eql(tripData);
+  });
 
-  it('should find and store all trips for one user', () => {
-    expect(trip1.tripDataOneUser.length).to.equal(4)
-  })
+  it("should find and store all trips for one user", () => {
+    expect(trip1.tripDataOneUser.length).to.equal(4);
+  });
 
-  it('should return a singlue users destinationID for all trips theyve taken', () => {
-    expect(trip1.returnDestinationId()).to.eql([49, 33, 14, 41])
-  })
+  it("should return a singlue users destinationID for all trips theyve taken", () => {
+    expect(trip1.returnDestinationId()).to.eql([49, 33, 14, 41]);
+  });
 
-  it('should caluculate cost of a single trip picked by trip ID', () => {
-    expect(trip1.getCostOfOneTrip(68, 500, 100)).to.equal(7600)
-  })
+  it("should caluculate cost of a single trip picked by trip ID", () => {
+    expect(trip1.getCostOfOneTrip(68, 500, 100)).to.equal(7600);
+  });
 
-  it('should calculate all trips taken this year', () => {
-    expect(trip1.getCostOfTripsThisYear(destinationData)).to.equal(5819)
-    expect(trip2.getCostOfTripsThisYear(destinationData)).to.equal(16368)
-  })
+  it("should calculate all trips taken this year", () => {
+    expect(trip1.getCostOfTripsThisYear(destinationData)).to.equal(5819);
+    expect(trip2.getCostOfTripsThisYear(destinationData)).to.equal(16368);
+  });
 
+  it("should reformat all the dates into numbers", () => {
+    expect(trip3.turnDatesToNumber()[0].date).to.be.a("number");
+    expect(trip3.tripDataNumberDates[1].date).to.be.a("number");
+  });
 
+  it("should sort one users trips into past, present and pending", () => {
+    let result = trip3.sortedTrips();
+    expect(result).to.be.an("object");
+    expect(result.past.length).to.equal(1);
+    expect(result.pending.length).to.equal(2);
+  });
 });
