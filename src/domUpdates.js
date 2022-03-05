c259258f419bcc075b8c32dd68d96eef3a965062
Emili Kaiman <emilikaiman@gmail.com>
2022-03-05 13:50:22 -0700

    Update dom by past pending and future trips

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,32 +1,39 @@
 const domUpdates = {
   updateTotalSpent(total) {
     let totalSpent = document.getElementById("totalSpent");
-    totalSpent.innerText = ''
+    totalSpent.innerText = "";
     totalSpent.innerText = ` Total Spent this Year: $${total}`;
   },
 
   displayAllTrips(allTrips, destinationData) {
-    const pastTrips = document.getElementById("pastTrips")
-    const pendingTrips = document.getElementById("pendingTrips")
-    const futureTrips = document.getElementById("futureTrips")
-    pastTrips.innerHTML = ''
-    pendingTrips.innerHTML = ''
-    futureTrips.innerHTML = ''
-    allTrips.pending.forEach((trip) => {
-      destinationData.forEach((destination) => {
-        if (trip.destinationID === destination.id) {
-          pendingTrips.innerHTML += `<div class="oneTrip square">
-          <p id="destinatio">${destination.destination}</p>
+    const pastTrips = document.getElementById("pastTrips");
+    const pendingTrips = document.getElementById("pendingTrips");
+    const futureTrips = document.getElementById("futureTrips");
+    pastTrips.innerHTML = "";
+    pendingTrips.innerHTML = "";
+    futureTrips.innerHTML = "";
+
+    if (allTrips.pending.length === 0) {
+      pendingTrips.innerHTML += `<div class="oneTrip square">
+      <p id="destination"> You have no pending trips</p>
+      </div>`;
+    } else {
+      allTrips.pending.forEach((trip) => {
+        destinationData.forEach((destination) => {
+          if (trip.destinationID === destination.id) {
+            pendingTrips.innerHTML += `<div class="oneTrip square">
+          <p id="destination">${destination.destination}</p>
           <img src='${destination.image}' alt="${destination.alt}">
           </div>`;
-        }
+          }
+        });
       });
-    });
+    }
     allTrips.past.forEach((trip) => {
       destinationData.forEach((destination) => {
         if (trip.destinationID === destination.id) {
           pastTrips.innerHTML += `<div class="oneTrip square">
-          <p id="destinatio">${destination.destination}</p>
+          <p id="destination">${destination.destination}</p>
           <img src='${destination.image}' alt="${destination.alt}">
           </div>`;
         }
@@ -46,15 +53,19 @@ const domUpdates = {
 
   displayDropDownOptions(destinationData) {
     let dropDownDestinations = document.getElementById("dropDownDestinations");
-    destinationData.forEach(destination => {
+    dropDownDestinations.innerHTML =
+      '<option value="" disabled selected hidden>destinations</option>';
+    destinationData.forEach((destination) => {
       dropDownDestinations.innerHTML += `<option value="${destination.destination}">${destination.destination}</option>`;
     });
   },
 
   displayEstimatedCost(total, agentFee) {
-    let estimatedCost = document.getElementById("estimatedCost")
-    estimatedCost.innerText = `You're estimated cost is ${total} plus an agent fee of ${agentFee} for a total of ${total + agentFee}`
-  }
+    let estimatedCost = document.getElementById("estimatedCost");
+    estimatedCost.innerText = `You're estimated cost is ${total} plus an agent fee of ${agentFee} for a total of ${
+      total + agentFee
+    }`;
+  },
 };
 
 export default domUpdates;
